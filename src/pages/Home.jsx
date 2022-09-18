6e98fb2e33b3f1f37a37f10fb723a8b225dbf6ba
Elishaokon13 <eliscodavid@gmail.com>
2022-09-18 20:33:04 +0100

    upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import HomeHero from '../components/HomeHero'
+import Navbar from '../components/Navbar'
+import Features from '../components/Features'
+
+function Home() {
+    return (
+      <div>
+    
+            <HomeHero />
+            <Features />
+            
+        </div>
+    )
+}
+
+export default Home
