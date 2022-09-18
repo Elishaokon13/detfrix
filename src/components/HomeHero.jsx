d514fa48661accfadd7a84555bb4cabe04095226
Elishaokon13 <eliscodavid@gmail.com>
2022-09-19 00:10:19 +0100

    edit

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -64,7 +64,7 @@ function HomeHero() {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full"
-          src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
+          src="https://codeo.vercel.app/_nuxt/img/hero-image.15eeae4.webp"
           alt=""
         />
       </div>
