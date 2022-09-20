f4bc6d7b0b07c414711891836d7c91e9ffadb461
Favour Ebrusike <ebrusikefavour@gmail.com>
2022-09-20 13:25:50 +0100

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors:{
+      white:'#FFFFFF',
+      blue:"#0057FF",
+      black:"#1E2125"
+    },
     extend: {},
   },
   plugins: [],
