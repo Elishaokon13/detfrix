1911018598ff040e0c864973d015985f193ff94c
Elisha Okon <63073316+Elishaokon13@users.noreply.github.com>
2022-09-20 14:45:52 +0100

    Update HomeHero.jsx

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -51,7 +51,7 @@ function HomeHero() {
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <Link
                     to="#"
-                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-blue-100 px-8 py-3 text-base font-medium text-blue-700 hover:bg-blue-200 md:py-4 md:px-10 md:text-lg"
+                    className="flex w-full items-center justify-center rounded-md border border-transparent bg-blue-100 px-8 py-3 text-base font-medium text-blue-600 hover:bg-blue-200 md:py-4 md:px-10 md:text-lg"
                   >
                     Live demo
                   </Link>
