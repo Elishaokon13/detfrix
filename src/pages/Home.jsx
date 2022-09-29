0651f35811efce1bd14724dd7fbfc205e30728c6
eliscodavid@gmail.com <eliscodavid@gmail.com>
2022-09-29 12:39:29 +0100

    upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
             <Navbar />
             <HomeHero />
             <Features />
-            
+            <Plan />
             <Footer />
             
         </div>
