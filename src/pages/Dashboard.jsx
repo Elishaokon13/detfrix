2cb4534a881c29ca77e82d425d76668adde3e2b5
Elishaokon13 <eliscodavid@gmail.com>
2022-10-03 15:58:14 +0100

    edited

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,8 +4,9 @@ import Portfolio from "../portfolio/Portfolio"
 
 export default function Dashboard() {
     return (
-        <div>
-            <Sidebar />
+        <div className="grid grid-cols-3">
+        
+           <Sidebar />
             <Portfolio />
 
         </div>
--- src/portfolio/Portfolio.jsx
+++ src/portfolio/Portfolio.jsx
@@ -2,27 +2,25 @@ import React from "react"
 
 export default function Portfolio() {
     return (
-        <div className=" gap-5 items-left justify-left w-full bg-gray-100 dark:bg-gray-900">
-            <h1 className=" flex flex-row text-2xl mr-screen px-25 font-semibold text-gray-700 dark:text-gray-200">
-                Portfolio
-            </h1>
+        <div className="grid grid-cols gap-5 p-70 items-center -right-23 justify-center w-full bg-gray-100 dark:bg-gray-900">
+            
 
 
-            <a href="#" class="block flex flex-col gap-5 item-start p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-                <h5 class="mb-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">$1500</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+            <a href="#" class="block flex flex-col gap-5  item-start p-6 max-w-2xl bg-white rounded-lg border border-gray-200 shadow-md hover:bg-blue-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <h5 class="  mb-2 text-2xl text-center font-bold tracking-tight text-gray-900 dark:text-white">$1500</h5>
+                <p class="font-normal text-sm text-center text-gray-700 dark:text-gray-400">Total Balance</p>
             </a>
 
 
-            <a href="#" class="block flex flex-col gap-5 p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <a href="#" class="block flex flex-col gap-5 p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-blue-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <h5 class="mb-2 text-2xl font-bold text-center tracking-tight text-gray-900 dark:text-white">$1500</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <p class="font-normal text-gray-700 dark:text-gray-400">Total Balance</p>
             </a>
 
 
-            <a href="#" class="block p-6 max-w-sm gap-5 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <a href="#" class="block p-6 max-w-sm gap-5 flex flex-col bg-white rounded-lg border border-gray-200 shadow-md hover:bg-blue-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <h5 class="mb-2 text-2xl font-bold text-center tracking-tight text-gray-900 dark:text-white">$1500</h5>
-                <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                <p class="font-normal text-gray-700 dark:text-gray-400">Total Balance</p>
             </a>
 
 
