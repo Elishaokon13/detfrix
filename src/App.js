65b1b03c6b52c698d57ae28c59d8aca2dc50de53
eliscodavid@gmail.com <eliscodavid@gmail.com>
2022-10-04 04:10:00 +0100

    dashboard Layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
 import SignIn from './pages/SignIn'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
     <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignIn />} />
+          <Route path="/dashboard" element={<Dashboard/>} />
           <Route path="*" element={<NotFound />} />
 
         </Routes>
