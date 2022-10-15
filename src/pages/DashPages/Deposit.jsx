e64626e8fa91c45a9b583de27cfdd2b0d72e1647
Elishaokon13 <eliscodavid@gmail.com>
2022-10-15 09:10:00 +0100

    topnaf

--- src/pages/DashPages/Deposit.jsx
+++ src/pages/DashPages/Deposit.jsx
@@ -35,7 +35,7 @@ function Deposit() {
       </div>
 
       <> 
-        <div class="flex justify-center mt-10 items-center w-40">
+        <div class="flex justify-center mt-10  h-screen items-center w-60 md:w-80">
             <label for="dropzone-file" class="flex flex-col justify-center items-center w-full h-64 bg-blue-300 rounded-lg border-2 border-blue-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-blue-700 hover:bg-blue-100 dark:border-blue-600 dark:hover:border-blue-500 dark:hover:bg-blue-600">
         <div class="flex flex-col justify-center items-center pt-5 pb-6">
             <svg aria-hidden="true" class="mb-3 w-10 h-10 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
