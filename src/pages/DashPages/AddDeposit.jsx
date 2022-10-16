ecaf7a1df5b7e18371c6817d0b83b502376059cf
Favour Ebrusike <ebrusikefavour@gmail.com>
2022-10-16 22:13:18 +0100

    add deposit

--- src/pages/DashPages/AddDeposit.jsx
+++ src/pages/DashPages/AddDeposit.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import DepositService from '../../services/deposit.service';
 const AddDeposit = () => {
+    const intialDepositState = {
+        amount:"",
+        paymentMethod:"",
+        upload:""
+    };
+    const [deposit, setDeposit] = useState(intialDepositState)
+    const [submitted, setSubmitted] = useState(false);
+    const handleInputChange = event => {
+        const { name, value } = event.target;
+        setDeposit({ ...deposit, [name]: value });
+      };
+
+      const saveDeposit = () => {
+        var data = {
+            amount: deposit.amount,
+            paymentMethod: deposit.paymentMethod,
+            upload: deposit.upload
+        };
+    
+        DepositService.create(data)
+          .then(response => {
+            setDeposit({
+             
+                amount: response.data.title,
+                paymentMethod: response.data.description,
+                upload: response.data.published
+            });
+           console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+
+         const newDeposit = () =>{
+            setDeposit(intialDepositState);
+          }
+      };
     return (
-    <div>AddDeposit</div>
+    <div>
+
+    </div>
     )
 }
 
