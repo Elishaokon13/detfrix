9450b94987739624419882915dadcf66041beb7a
Precious Affiah <preciousaffiah205@gmail.com>
2022-10-18 00:07:21 +0100

    Done most parts of darshboard

--- src/components/DashComponents/ReferralBox.jsx
+++ src/components/DashComponents/ReferralBox.jsx
@@ -21,8 +21,8 @@ const ReferralBox = () => {
    }
 
    return (
-      <div className="p-3 w-50 font-[roboto] rounded bg-blue-100 border border-black">
-         <h3 className="text-lg text-center font-semibold">Referral Link</h3>
+      <div className="p-3 w-50 font-[roboto] rounded bg-white border border-black">
+         <h3 className="text-lg text-start font-semibold">Referral Link</h3>
          <p className="py-2 text-sm">
             Automatically top up your <br/> account balance by sharing your Referral
             link. earn a percentage for whatever plan your Referred user buy.
@@ -31,13 +31,13 @@ const ReferralBox = () => {
          <div className="flex items-center border-b-2 border-gray-700 pb-1.5 cursor-pointer">
             <input
                ref={textAreaRef}
-               className="mr-3 font-semibold border-none focus:outline-none selection:bg-transparent w-80"
+               className="mr-3 font-semibold text-sm border-none focus:outline-none selection:bg-transparent w-80"
                contentEditable={false}
                defaultValue="https://detfrix.com/referral/user"
             />
             <button className="flex items-center" onClick={(e) => onCopy(e)}>
-               <FaCopy className="h-6 w-6" />
-               <p className="ml-2 text-white uppercase rounded-md border border-transparent bg-blue-600 font-medium">copy</p>
+               <FaCopy className="h-4 w-5" />
+               <p className="ml-2 text-blue-400 uppercase rounded-md border border-transparent text-sm">copy</p>
             </button>
          </div>
          {copyStatus && (
