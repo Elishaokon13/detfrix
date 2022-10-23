76ac2dcc3c846d50f5aab2b09c91c5182e42c88f
Elisha Okon <63073316+Elishaokon13@users.noreply.github.com>
2022-10-23 11:39:53 +0100

    Create AddDeposit.jsx

---
+++ src/pages/DashPages/AddDeposit.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import { createDeposit } from '../../action/deposit';
+import DepositService from '../../services/deposit.service';
+const AddDeposit = () => {
+    const intialDepositState = {
+        amount:"",
+        paymentMethod:"",
+        upload:""
+    };
+   
+    const handleInputChange = event => {
+        const { name, value } = event.target;
+        setDeposit({ ...deposit, [name]: value });
+      };
+
+      const saveDeposit = () => {
+        var data = {
+            amount: deposit.amount,
+            paymentMethod: deposit.paymentMethod,
+            upload: deposit.upload
+        };
+    
+        createDeposit(data)
+          .then(response => {
+            setDeposit({
+             
+                amount: response.data.title,
+                paymentMethod: response.data.description,
+                upload: response.data.published
+            });
+           console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+
+         const newDeposit = () =>{
+            setDeposit(intialDepositState);
+          }
+      };
+    return (
+    <div>
+
+    </div>
+    )
+}
+
+export default AddDeposit
