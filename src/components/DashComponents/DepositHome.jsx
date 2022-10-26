7a1c15af3b899b1b97c1122838c0bcedf78c9570
Precious Affiah <preciousaffiah205@gmail.com>
2022-10-26 01:29:47 +0100

    done with deposit

---
+++ src/components/DashComponents/DepositHome.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { FaArrowCircleDown } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+import Bottomnav from "./Bottomnav";
+import DepositUsdt from "./DepositUsdt";
+
+function DepositHome() {
+    const [modal1, setModal1] = useState(false);
+    return (
+        <div>
+
+            <section>
+                <section className='w-full h-full  lg:px-8 lg:py-6 p-0'>
+
+                    <div className="lg:gap-3 mt-3 h-2/3 pb-10">
+                        <p className="mx-2 font-semibold text-lg pb-6">Select withdrawal method</p>
+
+                        <div className="py-3 mt-3 h-full lg:bg-gray-200 bg-white pb-10 rounded-lg lg:w-full" >
+                            <div className=" h-full px-5 gap-5 cursor-pointer" onClick={() => setModal1(!modal1)}>
+                                <div className="w-full">
+                                    <div className="flex items-center gap-4 px-5 py-2 rounded-sm bg-white w-full mb-2 hover:bg-gray-50 lg:border-0 border-[0.1px] border-gray-200" >
+                                        <FaArrowCircleDown className="w-6 h-5 text-green-500" />
+                                        <div className="w-1/2">
+                                            <p>USDT</p>
+                                            <ul className="flex flex-row gap-2 text-sm">
+                                                <li>
+                                                    <p>$57.155.40</p>
+                                                </li>
+                                                <li className="text-green-500">
+                                                    <p>+2.34%</p>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                        <div className="w-1/2 flex justify-end">
+                                            <FaArrowRight className="h-5 w-5" />
+                                        </div>
+                                    </div>
+
+
+                                </div>
+
+                            </div>
+                        </div>
+
+
+                    </div>
+                </section>
+                <section>
+                    <Bottomnav />
+                </section>
+                <section>
+                    {modal1 ?
+                        <DepositUsdt/>
+                        :
+                        <p></p>
+                    }
+                </section>
+            </section>
+
+        </div>
+    )
+}
+export default DepositHome;
--- src/pages/DashPages/Deposit.jsx
+++ src/pages/DashPages/Deposit.jsx
@@ -1,57 +1,10 @@
 import React from 'react'
+import DepositHome from '../../components/DashComponents/DepositHome'
 
 
 function Deposit() {
   return (
-    <div className='flex flex-col bg-blue-100 h-screen items-center justify-center'>
-      <div className='text-[45px] md:text-[60px] font-[roboto] font-[700]'>Deposit</div>
-      <div className='mt-[20px] flex flex-col items-center justify-center'>
-        <div className='flex flex-col items-center justify-center'>
-          <div className='text-[15px] font-[roboto] font-[700]'>Deposit Amount</div>
-          <div className='mt-[10px] flex items-center justify-center'>
-            <div className='bg-[#F5F5F5] h-[40px] w-[40px] flex items-center justify-center'>
-              <div className='text-[20px] font-[roboto] font-[700]'>-</div>
-            </div>
-            <div className='bg-[#F5F5F5] h-[40px] w-[40px] flex items-center justify-center'>
-              <div className='text-[20px] font-[roboto] font-[700]'>+</div>
-            </div>
-          </div>
-        </div>
-        <div className='mt-[20px] flex flex-col items-center justify-center'>
-          <div className='text-[15px] font-[roboto] font-[700]'>Payment Method</div>
-          <div className='mt-[10px] flex items-center justify-center'>
-            <div className='bg-[#F5F5F5] h-[40px] w-[130px] flex items-center justify-center'>
-              <div className='text-[20px] font-[roboto] font-[700]'>2361074768</div>
-            </div>
-            <div className='bg-[#F5F5F5] h-[40px] w-[100px] flex items-center  justify-center ml-8'>
-              <div className='text-[20px] font-[roboto] font-[700] '>PAGA MFB</div>
-            </div>
-          </div>
-        </div>
-        <div className='mt-[20px] flex flex-col items-center mt-12 justify-center'>
-          <div className='text-[15px] font-[roboto] font-[700]'>Upload Your Proof Of Payment</div>
-          
-        </div>
-      </div>
-
-      <> 
-        <div class="flex justify-center mt-10  h-screen items-center w-60 md:w-80">
-            <label for="dropzone-file" class="flex flex-col justify-center items-center w-full h-64 bg-blue-300 rounded-lg border-2 border-blue-300 border-dashed cursor-pointer dark:hover:bg-bray-800 dark:bg-blue-700 hover:bg-blue-100 dark:border-blue-600 dark:hover:border-blue-500 dark:hover:bg-blue-600">
-        <div class="flex flex-col justify-center items-center pt-5 pb-6">
-            <svg aria-hidden="true" class="mb-3 w-10 h-10 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-            <p class="mb-2 text-sm text-blue-500 dark:text-blue-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-            <p class="text-xs text-blue-500 dark:text-blue-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-        </div>
-        <input id="dropzone-file" type="file" class="hidden"/>
-    </label>
-</div>
-</>
-
-    </div>
-
-
-
-
+      <DepositHome/>
   )
 }
 
